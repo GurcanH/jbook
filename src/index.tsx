cb37558d0427088d233858643ae754f0b9e708d5
GurcanH <gurcanhamali@gmail.com>
2021-03-17 21:14:05 +1100

    059. Understanding ESBuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Hi Gurcan!</h1>;
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+        <pre>{code}</pre>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
