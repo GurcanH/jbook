0f5ae0204a7469c48f1dae85272b8c8e8e86fba0
GurcanH <gurcanhamali@gmail.com>
2021-03-21 21:04:40 +1100

    210. Last Touch of Styling

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -11,16 +11,23 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === 'code') {
-    child = <CodeCell cell={cell} />;
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <ActionBar id={cell.id} />
+        <TextEditor cell={cell} />
+      </>
+    );
   }
-  return (
-    <div className="cell-list-item">
-      <ActionBar id={cell.id} />
-      {child}
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
